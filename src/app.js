1d1814f007cbdacdd93ed5389e4d2b3b837e09a6
MCVdev <mateocarvajalvergara333@gmail.com>
2025-01-15 00:29:22 -0500

    cors for dev

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = require("express")();
+const cors = require("cors");
 
 const influencerRouters = require("./routes/influencerRoutes");
 
+app.use(cors())
 app.use(express.json());
+
 
 // Routes
 app.use("/api/influencer", influencerRouters);
